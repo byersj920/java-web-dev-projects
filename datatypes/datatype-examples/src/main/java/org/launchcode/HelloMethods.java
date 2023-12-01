dfed6e41e34b5942927b92addc470c1988f938d0
Jonathan <byersj920@gmail.com>
2023-11-30 19:00:31 -0600

    Lecture Practice

--- datatypes/datatype-examples/src/main/java/org/launchcode/HelloMethods.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/HelloMethods.java
@@ -3,8 +3,15 @@ package org.launchcode;
 public class HelloMethods {
 
     public static void main(String[] args) {
-        String message = Message.getMessage("fr");
-        System.out.println(message);
+        /*String message = Message.getMessage("fr");
+        System.out.println(message);*/
+
+        StaticMouse m = new StaticMouse();
+        System.out.println(m.weight);
+        m.weight = 95;
+        System.out.println(m.weight);
+
+        m.squeak();
     }
 
 }
---
+++ datatypes/datatype-examples/src/main/java/org/launchcode/StaticMouse.java
@@ -0,0 +1,8 @@
+package org.launchcode;
+
+public class StaticMouse {
+    public double weight = 1.5;
+    public void squeak(){
+        System.out.println("I am so down for cheese, dude.");
+    }
+}
