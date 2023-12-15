ef75488252e71b3681c00946b76cfa1c0ebe9a27
Jonathan <byersj920@gmail.com>
2023-12-14 19:01:33 -0600

    Exercise Submission

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -26,6 +26,21 @@ class CarTest {
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+    @Test
+    public void testTankLevelPostDrive(){
+        test_car.drive(50);
+        assertEquals(9,test_car.getGasTankLevel(),.001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    @Test
+    public void testDrivingTooFar(){
+        test_car.drive(50000000);
+        assertEquals(0,test_car.getGasTankLevel(),.001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+    @Test
+    public void testGasOverfillException(){
+        assertThrows(IllegalArgumentException.class, ()->{test_car.addGas(5);},
+                "Car can't have any more gas! That trunk is too full of junk.");
+    }
 }
