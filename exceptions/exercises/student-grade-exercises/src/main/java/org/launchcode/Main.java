00cad95798262740b3642215c1e9a3b1a9be6869
Jonathan <byersj920@gmail.com>
2024-01-10 15:31:35 -0600

    Exercise Submission

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,29 +1,49 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
+        Divide(7,3);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
-        studentFiles.put("Elizabeth", "MyCode.java");
+        studentFiles.put("Elizabeth", "MyCode.jerva");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> student: studentFiles.entrySet()){
+            if (CheckFileExtension(student.getValue())==1){
+                System.out.printf("%s's is correct. They earn 1 point.%n", student.getKey());
+            } else if(CheckFileExtension(student.getValue())==0){
+                System.out.printf("%s's is incorrect. They earn 0 points.%n", student.getKey());
+            }
+        }
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        if (y == 0){
+            try{
+                throw new RuntimeException("You can't divide by zero, buddy.");
+            } catch(RuntimeException e){
+                e.printStackTrace();
+            }
+        }
     }
 
-    public static void CheckFileExtension(String fileName)
+    public static int CheckFileExtension(String fileName)
     {
-        //This code was popping up with an error. So I changed this method to static void instead
-        //of static int. Will have to change it back once I get to this chapter.
-        // Write code here!
+       if(fileName.isEmpty()){
+           try{
+               throw new RuntimeException("No file to read. Nothing submitted, you lose 1 point. :(");
+           } catch(RuntimeException e){
+               System.out.println("Error! No file submitted.");
+               return -1;}}
+       else if (fileName.endsWith(".java")){return 1;}
+       else{return 0;}
         };
+
 }
